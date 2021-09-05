eb52b2528227d7d1ae05337b2b081dd043969dcf
Vaibhav D S <vaibhavdinakar@gmail.com>
2021-09-05 11:34:27 +0530

    Added formatted alert

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function end(subs) {
     let now = 0
     // Completes all hard
     while ((subs[2].length) > 0) {
-        alert(subs[2][0][0]);
+        alert("Study " + subs[2][0][0] + " for " + subs[2][0][3] + "hours");
         subs[2].shift()
         if ((subs[now].length) > 0) {
             alert(subs[now][0][0]);
